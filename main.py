88d5f9f0c140858302e58bfdd659dfeeb63db96f
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-29 10:31:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,55 @@
 # Abraham Lincoln
 # 26 JUL 20XX
-# Project name goes here
+# Manipulating Strings
+
+# Part 1
+# Define a variable and assign it the string that is your last name
+# Then tell Python to print the value assigned to the variable
+
+
+
+
+
+
+
+# Part 2
+# Use the len( ) function to get the number of characters in your first name
+# Assign the length of your first name to a variable 
+# Have Python print the value stored in the variable
+
+
+
+
+
+
+
+# Part 3
+# Use the input ( ) and len ( ) functions to prompt (ask) the user to enter a first name
+# Then have Python print the length (number of characters) in the first name
+
+
+
+
+
+
+# Part 4
+# Assign the name of the city you live in to a variable called city
+# Use square bracket notation to extract (grab) and print the FOURTH character of your city
+
+
+
+
+
+# Part 5
+# Assign the string 'Career Tech' to a variable called school
+# Use square brackets and a starting and ending index number to print (display) only the characters 'Tec' on your screen when you run your script
+
+
+
+
+
+
+# Part 6
+# Assign the name of your favorite superhero or movie character to a variable
+# Use the Python upper ( ) string method to print the name of your superhero/movie character in uppercase 
+# Also use the Python lower ( ) string method to print the name of your superhero/movie character in lowercase 
